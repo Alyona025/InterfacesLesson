d634bd7209a6d81ae9266f932d42d7c77231132a
Alyona <gloria900dfg@gmail.com>
2025-08-03 21:22:53 +0900

    Добавление цикла и экземпляров классов

--- InterfacesLesson/Program.cs
+++ InterfacesLesson/Program.cs
@@ -11,13 +11,31 @@ namespace InterfacesLesson
         static void Main(string[] args)
         {
             Cow cow1 = new Cow();
+            Cow cow2 = new Cow();
+
             Car car1 = new Car(false);
+
             User user1 = new User("Гриша");
+            User user2 = new User("Маша");
+            User user3 = new User("Даша");
+            User user4 = new User("Ваня");
+            User user5 = new User("Саша");
 
             Console.WriteLine("Звуки: ");
-            cow1.MakeSound();
-            car1.MakeSound();
-            user1.MakeSound();
+
+
+            for (int i = 0; i < 1; i++ )
+            {
+                cow1.MakeSound();
+                cow2.MakeSound();
+                car1.MakeSound();
+                user1.MakeSound();
+                user2.MakeSound();
+                user3.MakeSound();
+                user4.MakeSound();
+                user5.MakeSound();
+
+            }
         }
     }
 }
